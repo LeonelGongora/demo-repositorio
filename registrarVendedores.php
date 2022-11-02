5659cb3f2eb686e411eaffb7fd28fe58bf0ddf0e
Leonel_Gongora <strokehead@outlook.es>
2022-11-02 18:50:13 -0400

    Prueba conexion base de datos

--- registrarVendedores.php
+++ registrarVendedores.php
@@ -5,8 +5,18 @@
 	$password = $_POST['password'];
 	$number = $_POST['number'];
 
+    //Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
 	// Database connection
-	$conn = new mysqli('localhost','root','strokes2500','test');
 	if($conn->connect_error){
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
@@ -14,13 +24,14 @@
 		$stmt = $conn->prepare("insert into registration(firstName, lastName,  email, password, number) values(?, ?, ?, ?, ?)");
 		$stmt->bind_param("ssssi", $firstName, $lastName, $email, $password, $number);
 		$execval = $stmt->execute();
-		//echo $execval;
-		//echo "Registration successfully...";
+		echo $execval;
+		echo "Registration successfully...";
 		$stmt->close();
 		$conn->close();
 	}
 
-    $url= 'ejemplo.php';
+    $url= 'prueba.php';
       echo '<META HTTP-EQUIV=REFRESH CONTENT="1; '.$url.'">';
 
 ?>
+
