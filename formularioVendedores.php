a7007af936bfbac47490960389bd9996fc0b249d
Leonel_Gongora <strokehead@outlook.es>
2022-11-04 05:02:00 -0400

    Pruebas 502

--- formularioVendedores.php
+++ formularioVendedores.php
@@ -26,7 +26,7 @@
             <h2>Registro de Vendedores</h2>
         </div class="alert alert-danger" role="alert">
             <div class="row justify-content-center my-5" style="font-family:Arial;font-size: 16px">
-                <form action="conexiones/registrarVendedores.php" method="post" enctype = "multipart/form-data" class="needs-validation" novalidate>
+                <form action="registrarVendedores.php" method="post" enctype = "multipart/form-data" class="needs-validation" novalidate>
                     <div class="row justify-content-center my-1" style=" margin-top: 50px; border-radius: 10px; box-shadow: 10px 10px 10px -6px black; background-color:white;">
                         <div class="col-lg-5">
                             
