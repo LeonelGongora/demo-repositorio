eb3a046181cd78aabd0239ddce1026d3da3a8acc
Leonel_Gongora <strokehead@outlook.es>
2022-11-04 00:54:22 -0400

    Prueba 324

--- miPerfil.php
+++ miPerfil.php
@@ -1,8 +1,4 @@
 <?php
-      
-
-      $resultado =  pg_fetch_all($tablaDeDatos);
-
     $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
     $cleardb_server = $cleardb_url["host"];
     $cleardb_username = $cleardb_url["user"];
@@ -10,7 +6,6 @@
     $cleardb_db = substr($cleardb_url["path"],1);
     $active_group = 'default';
     $query_builder = TRUE;
-    
     $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
     session_start();
