ddbbc3dba72b08c00500eca8aff78b8b905249e4
Leonel_Gongora <strokehead@outlook.es>
2022-11-04 00:45:37 -0400

    Prueba 303

--- miPerfil.php
+++ miPerfil.php
@@ -1,3 +1,33 @@
+<?php
+      
+
+      $resultado =  pg_fetch_all($tablaDeDatos);
+
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"],1);
+    $active_group = 'default';
+    $query_builder = TRUE;
+    
+    $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+    session_start();
+    $ema = $_SESSION['email1'];
+    $pas = $_SESSION['pass1'];
+
+    $result = mysqli_query($conn,"SELECT NombreNegocio, contrasenia, Email, Telefono, Ubicacion, ImagenVendedor FROM vendedores WHERE vendedores.Email = '$ema' AND vendedores.contrasenia = '$pas' LIMIT 1");
+
+    if(!$result){
+       echo "ocurrio un error";
+       exit;
+    }
+
+    for($i = 0; $resultado[$i] = mysqli_fetch_assoc($result); $i++) ;
+    // Delete last empty one
+    array_pop($resultado);      
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -25,12 +55,12 @@
         </div>
     </div>
 
-        
+        <?php if (is_array($resultado) || is_object($resultado)): {foreach($resultado as $row): ?>
           <div class="row justify-content-center my-2">
             <div class="col-lg-4">
               <div class="card mb-4 border-0" >
                 <div class="card-body text-center" >
-                  <img src="" width="200" height="200" alt="avatar"
+                  <img src="data:image/jpg;base64,<?php echo base64_encode($row['ImagenVendedor']);?>" width="200" height="200" alt="avatar"
                     class="rounded-0" style="width: 150px;">
                 </div>
               </div>
@@ -44,7 +74,7 @@
                       <p class="mb-0">Nombre de negocio:</p>
                     </div>
                     <div class="col-sm-9">
-                      <p class="text-muted mb-0"></p>
+                      <p class="text-muted mb-0"><?php echo $row['NombreNegocio'];?></p>
                     </div>
                   </div>
                   <hr>
@@ -53,7 +83,7 @@
                       <p class="mb-0">Email:</p>
                     </div>
                     <div class="col-sm-9">
-                      <p class="text-muted mb-0"></p>
+                      <p class="text-muted mb-0"><?php echo $row['Email'];?></p>
                     </div>
                   </div>
                   <hr>
@@ -62,7 +92,7 @@
                       <p class="mb-0">Teléfono:</p>
                     </div>
                     <div class="col-sm-9">
-                      <p class="text-muted mb-0"></p>
+                      <p class="text-muted mb-0"><?php echo $row['Telefono'];?></p>
                     </div>
                   </div>
                   <hr>
@@ -71,7 +101,7 @@
                       <p class="mb-0">Ubicación:</p>
                     </div>
                     <div class="col-sm-9">
-                      <p class="text-muted mb-0"></p>
+                      <p class="text-muted mb-0"><?php echo $row['Ubicacion'];?></p>
                     </div>
                   </div>
 
@@ -81,9 +111,10 @@
                       <p class="mb-0">Descripción:</p>
                     </div>
                     <div class="col-sm-9">
-                      <p class="text-muted mb-0"></p>
+                      <p class="text-muted mb-0"><?php echo $row['Descripcion'];?></p>
                     </div>
                   </div>
+                  <?php endforeach;} endif;?>
                 </div>
               </div>
             </div>
@@ -93,11 +124,11 @@
         <div class="text-center">
             <div class="row justify-content-center my-5">
                 <div class="col-2">
-                    <a href="paginaPrincipalVendedores.php" class="btn btn-danger rounded-0" role="button">Volver</a>
+                    <a href="paginaPrincipalConPerfil.php" class="btn btn-danger rounded-0" role="button">Volver</a>
                 </div>
                 
                 <div class="col-2">
-                    <a href="editarPerfil.php" class="btn btn-success" role="button">Editar Perfil</a>
+                    <a href="editarPerfilVendedor.php" class="btn btn-success" role="button">Editar Perfil</a>
                 </div>
                 
                 <div class="col-2">
