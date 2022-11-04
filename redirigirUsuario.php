db3fb310d9dae3bbc4a8b00713b58f1d74bfa7f9
Leonel_Gongora <strokehead@outlook.es>
2022-11-03 21:56:19 -0400

    Pruena 100

--- redirigirUsuario.php
+++ redirigirUsuario.php
@@ -1,14 +1,24 @@
 <?php
-    require 'conexion.php';
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"],1);
+    $active_group = 'default';
+    $query_builder = TRUE;
     session_start();
+
+    $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+
     $_SESSION['email1'] = $_POST['Email'];
     $_SESSION['pass1'] = $_POST['password'];
 
     $emailactual = $_SESSION['email1'];
     $passactual = $_SESSION['pass1'];
 
-    $consultaVendedores = mysqli_query($conexion,"SELECT * FROM vendedores WHERE Email = '$emailactual' AND vendedores.password = '$passactual' LIMIT 1");
-    $consultaCompradores = mysqli_query($conexion,"SELECT * FROM compradores WHERE Email = '$emailactual' AND compradores.password = '$passactual' LIMIT 1");
+    $consultaVendedores = mysqli_query($conn,"SELECT * FROM vendedores WHERE Email = '$emailactual' AND vendedores.password = '$passactual' LIMIT 1");
+    $consultaCompradores = mysqli_query($conn,"SELECT * FROM compradores WHERE Email = '$emailactual' AND compradores.password = '$passactual' LIMIT 1");
 
     
 
