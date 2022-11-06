ae9855970c0af336a8ebdfd177d3ac7e61807394
Leonel_Gongora <strokehead@outlook.es>
2022-11-06 05:45:18 -0400

    Pruebas2324

--- javascript/validacion.js
+++ javascript/validacion.js
@@ -10,6 +10,7 @@
     (function () {
     'use strict'
     let pattern1 = /^[A-Z|a-z|0-9|`|&|.|\s|!|-|,]{3,20}$/; 
+    let pattern2 = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/; 
 
     var forms = document.querySelectorAll('.needs-validation')
 
@@ -18,6 +19,10 @@
         form.addEventListener('submit', function (event) {
           var formulario = form.getElementsByTagName("input");
           if (!formulario[0].value.match(pattern1)) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+          if (!formulario[1].value.match(pattern2)) {
             event.preventDefault()
             event.stopPropagation()
           }
