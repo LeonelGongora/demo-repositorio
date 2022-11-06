1383c416ef2cf04742e7e8285638d597214e3bc3
Leonel_Gongora <strokehead@outlook.es>
2022-11-06 07:08:22 -0400

    PRJS435

--- formularioVendedoresAux.php
+++ formularioVendedoresAux.php
@@ -46,7 +46,7 @@
                             
                           <div class="col">
                               <label for="NombreNegocio" class="form-label">Nombre de Negocio:</label>
-                               <input type="text" name="NombreNegocio" id="NombreNegocio"class="form-control" required>
+                               <input type="text" name="NombreNegocio" id="NombreNegocio"class="form-control" pattern = "^[A-Z|a-z|0-9|`|&|.|\s|!|-|,]{3,20}$" required>
                              <div id = "validacion1" class="invalid-feedback">
                                Ingrese un nombre valido
                              </div>
--- javascript/validacion.js
+++ javascript/validacion.js
@@ -14,7 +14,7 @@
           var alertas = form.getElementsByClassName("invalid-feedback");
           console.log(alertas);
 
-          if (!formulario[0].value.match(pattern1)) {
+          if (!formulario[0].value.length == 0) {
             document.getElementById("validacion1").innerText = "Este campo es obligatorio";
 
             event.preventDefault()
