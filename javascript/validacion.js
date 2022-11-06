224c5fe3e679051aabb9d24df6937f4a7ebf1353
Leonel_Gongora <strokehead@outlook.es>
2022-11-06 05:31:05 -0400

    Pruebas

--- javascript/validacion.js
+++ javascript/validacion.js
@@ -11,20 +11,20 @@
     'use strict'
 
     var forms = document.querySelectorAll('.needs-validation')
-    console.log(forms);
 
     Array.prototype.slice.call(forms)
         .forEach(function (form) {
         form.addEventListener('submit', function (event) {
+          valores = form.getElementsByTagName("input");
 
           console.log(forms)
           console.log(form)
+          console.log()
           console.log(event)
           
           if (!form.checkValidity()) {
             event.preventDefault()
             event.stopPropagation()
-            console.log(form);
           }
           form.classList.add('was-validated')
         }, false)
