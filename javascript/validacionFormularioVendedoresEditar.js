5c0dea97858a64aa3583f63fd805f37daa797d0f
Leonel_Gongora <strokehead@outlook.es>
2022-11-08 03:40:51 -0400

    jkjsds

--- javascript/validacionFormularioVendedoresEditar.js
+++ javascript/validacionFormularioVendedoresEditar.js
@@ -1,77 +1,89 @@
 
     (function () {
 
-        let pattern1 = /^[A-Z|a-z|0-9|`|&|.|\s|!|-|,]{3,20}$/; 
-        let pattern2 = /^[A-Z|a-z|0-9|&|$|@|-|%|*|#|,|.|;|+|/]{6,14}$/; 
-        let pattern3 = /[7|6][0-9]{7}$/;
-        let pattern4 = /https:\/\/goo.gl\/maps+\/\w+|https:\/\/maps.app.goo.gl\/\w+/;
+        imagen = document.getElementById("ImagenVendedor")
+  
+      let pattern1 = /^[A-Z|a-z|0-9|`|&|.|\s|!|-|,]{3,20}$/; 
+      let pattern2 = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/; 
+      let pattern3 = /^[A-Z|a-z|0-9|&|$|@|-|%|*|#|,|.|;|+|/]{6,14}$/; 
+      let pattern4 = /[7|6][0-9]{7}$/;
+      let pattern5 = /https:\/\/goo.gl\/maps+\/\w+|https:\/\/maps.app.goo.gl\/\w+/;
+  
+      var forms = document.querySelectorAll('.needs-validation')
+  
+      Array.prototype.slice.call(forms)
+          .forEach(function (form) {
+          form.addEventListener('submit', function (event) {
+            var formulario = form.getElementsByTagName("input");
+            var alertas = form.getElementsByClassName("invalid-feedback");
+            console.log(formulario);
+  
+            if (!formulario[0].value.match(pattern1)) {
+              event.preventDefault()
+              event.stopPropagation()
+              document.getElementById("validacion1").innerText = "Ingrese un nombre valido";
+              if(formulario[0].value == ""){
+                document.getElementById("validacion1").innerText = "Este campo es obligatorio";
     
-        var forms = document.querySelectorAll('.needs-validation')
-    
-        Array.prototype.slice.call(forms)
-            .forEach(function (form) {
-            form.addEventListener('submit', function (event) {
-              var formulario = form.getElementsByTagName("input");
-              var alertas = form.getElementsByClassName("invalid-feedback");
-              console.log(formulario);
-    
-    
-              if (!formulario[0].value.match(pattern1)) {
-                event.preventDefault()
-                event.stopPropagation()
-                document.getElementById("validacion1").innerText = "Ingrese un nombre valido";
-                console.log("aaaaa");
-                if(formulario[0].value == ""){
-                  console.log("ssada");
-                  document.getElementById("validacion1").innerText = "Este campo es obligatorio";
-      
-                }
               }
-    
+            }
+  
+            if (!formulario[1].value.match(pattern2)) {
+              event.preventDefault()
+              event.stopPropagation()
+              document.getElementById("validacion2").innerText = "Ingrese un email valido";
+              if(formulario[1].value == ""){
               
-              if (!formulario[1].value.match(pattern2)) {
-                event.preventDefault()
-                event.stopPropagation()
-                document.getElementById("validacion2").innerText = "La contraseña debe tener un minimo de 6 caracteres y maximo 14";
-                console.log("aaaaa");
-                if(formulario[1].value == ""){
-                
-                  console.log("ssada");
-                  document.getElementById("validacion2").innerText = "Este campo es obligatorio";
-      
-                }
-              }
+                document.getElementById("validacion2").innerText = "Este campo es obligatorio";
     
-              if (!formulario[2].value.match(pattern3)) {
-                event.preventDefault()
-                event.stopPropagation()
-                document.getElementById("validacion3").innerText = "El teléfono debe tener 8 digitos y comenzar con el 6 o el 7";
+              }
+            }
+  
+            if (!formulario[2].value.match(pattern3)) {
+              event.preventDefault()
+              event.stopPropagation()
+              document.getElementById("validacion3").innerText = "La contraseña debe tener un minimo de 6 caracteres y maximo 14";
               
-                if(formulario[2].value == ""){
+              if(formulario[2].value == ""){
+              
+                document.getElementById("validacion3").innerText = "Este campo es obligatorio";
+    
+              }
+            }
+  
+            if (!formulario[3].value.match(pattern4)) {
+              event.preventDefault()
+              event.stopPropagation()
+              document.getElementById("validacion4").innerText = "El teléfono debe tener 8 digitos y comenzar con el 6 o el 7";
+            
+              if(formulario[3].value == ""){
+              
+                document.getElementById("validacion4").innerText = "Este campo es obligatorio";
                 
-                  document.getElementById("validacion3").innerText = "Este campo es obligatorio";
-                  
-                }
               }
+            }
+  
+            if (!formulario[4].value.match(pattern5)) {
+              event.preventDefault()
+              event.stopPropagation()
+              console.log("prueba")
+              document.getElementById("validacion5").innerText = "Ingrese una ubicación valida";
+              if(formulario[4].value == ""){
+              
+                document.getElementById("validacion5").innerText = "Este campo es obligatorio";
     
-              if (!formulario[3].value.match(pattern4)) {
-                event.preventDefault()
-                event.stopPropagation()
-                document.getElementById("validacion4").innerText = "Ingrese una ubicación valida";
-                if(formulario[3].value == ""){
-                
-                  document.getElementById("validacion4").innerText = "Este campo es obligatorio";
-      
-                }
               }
-
-              if (formulario[5].files.length == 0) {
-                event.preventDefault()
-                event.stopPropagation()
-              }
-    
-              form.classList.add('was-validated')
-    
-            }, false)
-          })
-      })()
+            }
+            console.log(formulario[6].files.length)
+  
+            if (formulario[6].files.length == 0) {
+              console.log("prueba imagen")
+              event.preventDefault()
+              event.stopPropagation()
+            }
+  
+            form.classList.add('was-validated')
+  
+          }, false)
+        })
+    })()
