4df0ce39f0312941b0eac1bfecbc2d0c871690bb
Leonel_Gongora <strokehead@outlook.es>
2022-11-07 23:30:08 -0400

    jkjskd

--- formularioVendedores.php
+++ formularioVendedores.php
@@ -151,7 +151,7 @@
     </div>
     <br>
   </section>
-    <script src="javascript/validacionFormularioVendedores.js"></script>
+    <script src="javascript/prueba1.js"></script>
     <script src="javascript/paraElOjo.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" 
     integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" 
