e509b01d1aa5acd48caecdc69ab36e7528053a6c
RaisaVO <115442239+RaisaVO@users.noreply.github.com>
2022-11-10 01:31:22 -0400

    Update formularioCompradoresFallido.php

--- formularioCompradoresFallido.php
+++ formularioCompradoresFallido.php
@@ -45,7 +45,7 @@
                   <div class="col">
                     <label for="NombreNegocio" class="form-label">Nombre:</label>
                     <input type="text" name="NombreNegocio" id="NombreNegocio" class="form-control"
-                      pattern="^[A-Z|a-z|0-9|`|&|.|\s|!|-|,]{3,20}$" required>
+                      pattern="^[A-Z|a-z|0-9|`|&|.|\s|!|-|,]{3,40}$" required>
                     <div id = "validacion1" class="invalid-feedback">
                       Ingrese un nombre valido
                     </div>
